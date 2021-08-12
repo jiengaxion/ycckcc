3cb2dccf969dcd01e24efea49c31567d405e4635
jiengaxion <59408559+jiengaxion@users.noreply.github.com>
2021-08-12 18:41:05 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
 $( "body" ).data( "foo", 52 );
 $( "body" ).data( "bar", { isManual: true } );
 $( "body" ).data( { baz: [ 1, 2, 3 ] } );
