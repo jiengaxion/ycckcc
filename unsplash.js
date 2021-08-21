612da19d47ac64d30691d96d74b164528a0d608b
jiengaxion <59408559+jiengaxion@users.noreply.github.com>
2021-08-21 20:28:10 +0800

    Update unsplash.js

--- unsplash.js
+++ unsplash.js
@@ -1,14 +1,19 @@
-$(document).ready(function(){
-  $("button").click(function(){
-    $.ajax({url: "https://api.unsplash.com/search/photos?query=ufo&client_id=N57ByMLZkjM4fgCN-5gjXkdP6q2SZLIk6J7jQfC5CuA&page=3&per_page=10", success: function(result){
+function pic(e){
+    $.ajax({url: "https://api.unsplash.com/search/photos?query=ufo&client_id=N57ByMLZkjM4fgCN-5gjXkdP6q2SZLIk6J7jQfC5CuA&page="+e+"&per_page=10", success: function(result){
    
     var string=""
+    var option=""
     for(i=0;i<result.results.length;i++){
     
     string=string+"<img src='"+result.results[i].urls.thumb+"'>";
+    if(i==e){
+    option=option+"<option value='"+i+"' selected>"+(i+1)+"</option>";
+    }else{
+    option=option+"<option value='"+i+"' >"+(i+1)+"</option>";
     }
     
+    }
+    string="<select onchange=pic(this.value);>"+option+"</select><br>"+string;
       $("#div1").html(string);
     }});
-  });
-});
+}pic(0)
