b9259ce5209e1fcda00ef4093072e62575974382
jiengaxion <59408559+jiengaxion@users.noreply.github.com>
2021-09-04 13:44:29 +0800

    Delete test.js

--- test.js
+++
@@ -1,6 +0,0 @@
-
-$( "body" ).data( "foo", 52 );
-$( "body" ).data( "bar", { isManual: true } );
-$( "body" ).data( { baz: [ 1, 2, 3 ] } );
-$( "body" ).data( "foo" ); // 52
-alert($( "body" ).data());
