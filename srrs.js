104cb9d3341e4dd4c3e8ab4a44438a19089f2493
jiengaxion <59408559+jiengaxion@users.noreply.github.com>
2021-09-19 13:36:07 +0800

    Update srrs.js

--- srrs.js
+++ srrs.js
@@ -1,6 +1,6 @@
 function srrs(a,b,c){
 
-          $.ajax({url: "https://script.google.com/macros/s/"+b+"/exec?item="+a+"&item2="+c, success: function(result){
+          $.ajax({url: "https://script.google.com/macros/s/AKfycbz8hIzsB4-mBxG4cjugsLgnRUl0eCU8piMVqYdWfljVubDmpc44/exec?item="+a+"&item2="+c, success: function(result){
             //alert(result)
   
 
