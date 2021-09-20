6519d891b88a87702a807fb530db4bab9105263e
jiengaxion <59408559+jiengaxion@users.noreply.github.com>
2021-09-20 10:15:12 +0800

    Create xhh.js

---
+++ xhh.js
@@ -0,0 +1,33 @@
+var y;       
+
+      function xhh(a,b){
+
+
+      
+
+
+       
+               $.ajax({url: "https://script.google.com/macros/s/AKfycbzl56qHFnWwzWwPcrvZi9Sxxi9gCPv9V7hUQkCVsUBRrf_C5wA/exec?item="+$("#xc").val()+"&item2="+a+"&item3="+b, success: function(result){
+      var Q =" ";            //alert(result)
+            var x=[result];
+                var obj =jQuery.parseJSON(x);
+
+               var Q=Object.values(obj)+Q;
+ //y1=Q.split('|~|');
+$( "#shopsss" ).slideUp( "slow", function() {
+      $("#shopsss").slideDown();
+$("#shopsss").html(Q);
+  });
+
+           
+
+                  }
+
+});
+
+              }
+
+function ho8(e){
+ $("#shopss").html(y1[e]);
+}
+
