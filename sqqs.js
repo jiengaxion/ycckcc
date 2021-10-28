a490e8bb1fe8d0dcc113e4e0ad2a721084649186
jiengaxion <59408559+jiengaxion@users.noreply.github.com>
2021-10-28 15:36:30 +0800

    Update sqqs.js

--- sqqs.js
+++ sqqs.js
@@ -1,5 +1,5 @@
 function sqqs(a){
-
+ $("#ten").html('<i class="w3-jumbo w3-spin fa fa-refresh"></i>');
           $.ajax({url: "https://script.google.com/macros/s/AKfycbx-izL5guK0W7SGraEP22mk0jCr2IudIvG0KWoCLSGT8AGGUco/exec?kk="+a, success: function(result){
             //alert(result)
   
