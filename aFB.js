64249448537c47e549ef35e999a7352face876fa
jiengaxion <59408559+jiengaxion@users.noreply.github.com>
2021-12-18 14:03:13 +0800

    Create aFB.js

---
+++ aFB.js
@@ -0,0 +1,14 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+
+<script>
+
+$(function(){
+if(location.search.split("=")[1].search('FB')>=0){
+    $.get({url: "https://script.google.com/macros/s/AKfycbz7B9pSiOLoHY11VcNlTOdvlL9KQ3x3eLxEz8jJMW7DvZBSNoo/exec", success: function(result){
+
+}});
+}
+   
+});
+</script>
